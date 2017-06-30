775a9dd2168ef987fcd8d75e009cd484f2e81a68
jm <michalak_jakub@yahoo.pl>
2017-06-30 12:06:32 +0200

    3 different types of scrolling events compared. chosen the final one. REQUEST FOR APPROVAL AND MERGE INTO DEVELOPE

--- contactPhotosTransition.js
+++ contactPhotosTransition.js
@@ -1,3 +1,32 @@
-$( "#about" ).mouseover(function() {
-    $( ".img-circle" ).addClass('active');
+/* SIMPLE VERSION CONNECTED TO THE MOUSE OVER
+
+$( '#about' ).mouseover(function() {
+    $( '.img-circle' ).addClass('active');
+});
+*/
+
+/* Y FIXED VERSION, not good for RWD
+
+$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y >= 1592) {
+        $('.img-circle').addClass('active');
+    }
+});
+
+*/
+
+$(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+        $('.img-circle').addClass('active');
+    }
+});
+
+
+
+/* PONIZEJ KOD WERYFIKUJACY DO USUNIECA W FINALNEJ WERSJI PROJEKTU */
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    console.log('pozycja scrolla' + scroll)
 });
