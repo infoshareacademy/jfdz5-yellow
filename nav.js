cd24e589f6af0af7cea8a984041a47366dfe7d7a
katarzynasitarz <katarzyna.sitarz@gmail.com>
2017-07-01 19:31:24 +0200

    FZ5YL-26 menu zwężające się - podejście drugie, menu się zwęża, a nie znika

--- nav.js
+++ nav.js
@@ -2,12 +2,23 @@
 /**
  * Created by turbokitty on 18.06.17.
  */
-var navbar = document.getElementById("navbar")
-document.addEventListener("wheel", function (event) {
-    if (event.wheelDelta < 0) {
-        navbar.classList.add("hidden")
-    } else {
-        navbar.classList.remove("hidden")
-    }
-    console.log(event.wheelDelta)
+$(document).ready(function() {
+        var navbar = document.getElementById("navbar")
+        document.addEventListener("wheel", function (event) {
+        if (event.wheelDelta < 0) {
+            navbar.classList.add("hidden1")
+        } else {
+            navbar.classList.remove("hidden1")
+        }
+        console.log(event.wheelDelta)
+    })
+
+
+    $("#kontakt").click(function () {
+        console.log($("#contact").offset());
+        $("html,body").animate({
+            scrollTop:$("#contact").offset().top
+        }, 5000)
+    })
+
 })
