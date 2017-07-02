7eb097b5f6a689186f9532d69aae66749828feb0
katarzynasitarz <katarzyna.sitarz@gmail.com>
2017-07-02 18:52:56 +0200

    FZ5YL-26 menu zwężające się - podejście drugie, menu się zwęża, a nie znika

--- nav.js
+++ nav.js
@@ -3,8 +3,8 @@
  * menu zwężające się  */
 
 $(document).ready(function() {
-        var navbar = document.getElementById("navbar")
-        document.addEventListener("wheel", function (event) {
+    var navbar = document.getElementById("navbar")
+    document.addEventListener("wheel", function (event) {
         if (event.wheelDelta < 0) {
             navbar.classList.add("hidden1")
         } else {
@@ -13,22 +13,34 @@ $(document).ready(function() {
         console.log(event.wheelDelta)
     })
 
-/* płynnie przejeżdżająca strona*/
+    /* płynnie przejeżdżająca strona*/
 
     $("#kontakt").click(function () {
         console.log($("#contact").offset());
         $("html,body").animate({
-            scrollTop:$("#contact").offset().top
+            scrollTop: $("#contact").offset().top
         }, 500)
     })
 
+    // $("#funkcje").click(function () {
+    //     console.log($("#contact").offset());
+    //     $("html,body").animate({
+    //         scrollTop: $("#contact").offset().top
+    //     }, 500)
+    // })
+})
 
 
 /* podświetlenie sekcji w menu */
 
 
 
-
-
-
-})
+           $(window).on('scroll', function () {
+            $('.target').each(function () {
+                if ($(window).scrollTop() >= $(this).position().top) {
+                    var id = $(this).attr('id');
+                    $('nav a').removeClass('nav__link__active');
+                    $('nav a[href="#' + id + '"]').addClass('nav__link__active');
+                }
+            });
+        });
