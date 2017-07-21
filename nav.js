92f3432b12c758fee63b3ce28ff2773b28fcda7e
katarzynasitarz <katarzyna.sitarz@gmail.com>
2017-07-21 21:24:59 +0200

    nav.js improved

--- nav.js
+++ nav.js
@@ -1,10 +1,28 @@
 
 /**
- * menu zwężające się  */
+ *
+ /* podświetlenie sekcji w menu */
+
+
+
+$(window).on('scroll', function () {
+    $('.target').each(function () {
+        if ($(window).scrollTop() >= $(this).position().top) {
+            var id = $(this).attr('id');
+            $('nav a').removeClass('nav__link__active');
+            $('nav a[href="#' + id + '"]').addClass('nav__link__active');
+        }
+    });
+});
+
+/* menu zwężające się  */
 
 $(document).ready(function() {
     var navbar = document.getElementById("navbar")
     document.addEventListener("wheel", function (event) {
+        if (window.innerWidth < 768) {
+            return false;
+        }
         if (event.wheelDelta < 0) {
             navbar.classList.add("small")
         } else {
