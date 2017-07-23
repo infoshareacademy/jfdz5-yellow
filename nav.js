e27c661e7bd643c9c3a62c08f8f1dd5f68bbecdd
katarzynasitarz <katarzyna.sitarz@gmail.com>
2017-07-23 20:44:14 +0200

    FZ5YL-26 poprawiona wersja dla firefoxa, podświetlane sekcje

--- nav.js
+++ nav.js
@@ -1,17 +1,26 @@
-
 /**
  * menu zwężające się  */
 
-$(document).ready(function() {
+var isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
+var scrollEvent = isFirefox ? 'DOMMouseScroll' : 'mousewheel';
+
+
+function onScroll(event) {
+    if (window.innerWidth < 768) {
+        return false;
+    }
+    var scrollDirection = event.wheelDelta || event.detail;
+    if (isFirefox ? scrollDirection > 0 : scrollDirection < 0) {
+        navbar.classList.add("small")
+    } else {
+        navbar.classList.remove("small")
+    }
+    console.log(event.wheelDelta, event.detail)
+}
+$(document).ready(function () {
     var navbar = document.getElementById("navbar")
-    document.addEventListener("wheel", function (event) {
-        if (event.wheelDelta < 0) {
-            navbar.classList.add("small")
-        } else {
-            navbar.classList.remove("small")
-        }
-        console.log(event.wheelDelta)
-    })
+    document.addEventListener(scrollEvent, onScroll)
+
 
     /* płynnie przejeżdżająca strona*/
 
@@ -22,19 +31,17 @@ $(document).ready(function() {
             scrollTop: $(hash).offset().top
         }, 500)
     })
+
+
+// * podświetlenie sekcji z menu */
+
+    $(".nav__link a").mouseover(function () {
+        console.log(this)
+        var id = $(this).attr('href');
+        var $element = $(id)
+        // console.log(element)
+        $element.parent("section").css("background-color", "red")
+
+    })
 })
 
-
-/* podświetlenie sekcji w menu */
-
-
-
-           $(window).on('scroll', function () {
-            $('.target').each(function () {
-                if ($(window).scrollTop() >= $(this).position().top) {
-                    var id = $(this).attr('id');
-                    $('nav a').removeClass('nav__link__active');
-                    $('nav a[href="#' + id + '"]').addClass('nav__link__active');
-                }
-            });
-        });
